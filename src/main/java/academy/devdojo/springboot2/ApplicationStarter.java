a06f5891440ab4de20412b8265fdfe23b62c6972
WelBert-dev <wellison.bertelli@hotmail.com>
2023-05-30 16:10:23 -0300

    Add aula04 - @Component, @Autowired, @SpringBootApplication (Formas de injetar dependências, Add Lombok no projeto e explica as 3 anotações basicas que formam um App Spring na classe main)

--- src/main/java/academy/devdojo/springboot2/ApplicationStarter.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStarter.java
@@ -1,11 +1,19 @@
-package academy.devdojo.springboot2.start;
+package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "academy.devdojo.springboot2")
+
+// Todos essas em uma unica abaixo:
+//@EnableAutoConfiguration
+//@ComponentScan
+//@Configuration
+
+// Aqui une as três anotações anteriores que formam a base de um APP Spring
+@SpringBootApplication
 public class ApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,17 +1,31 @@
 package academy.devdojo.springboot2.controller;
 
+import academy.devdojo.springboot2.util.DateUtil;
 import academy.devdojo.springboot2.domain.Anime;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 // COM Contexto anime:
+@Log4j2
 @RestController()
 @RequestMapping("anime")
+@RequiredArgsConstructor
 public class AnimeController {
+//    @Autowired
+    private final DateUtil dateUtil;
+
+//    public AnimeController(DateUtil dateUtil) {
+//        this.dateUtil = dateUtil;
+//    }
 
     // COM contexto:
     //localhost:8080/anime/list
@@ -20,6 +34,7 @@ public class AnimeController {
     // Novo jeito elegante:
     @GetMapping(path="list")
     public List<Anime> list() {
+        log.info("\n\n\nDATA ATUAL FORMATADA: "+ dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"),
                        new Anime("Berserk"));
     }
