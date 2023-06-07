ead7e109efe8ccb7c5783704434e4ab81e1351dc
WelBert-dev <wellison.bertelli@hotmail.com>
2023-06-07 14:29:38 -0300

    Add aula17 - Exceções Customizadas (BadRequestException extends RuntimeException com a annotation @ResponseStatus(HttpStatus.BAD_REQUEST) a nível de classe)

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,14 +1,13 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exceptions.httpRequest.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.anime.AnimePostRequestBodyDTO;
 import academy.devdojo.springboot2.requests.anime.AnimePutRequestBodyDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 @Service
@@ -23,8 +22,7 @@ public class AnimeService {
     }
     public Anime findByIdOrThrowBadRequestException(long id) {
         return this.animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                                                                "Anime Not Found"));
+                .orElseThrow(() -> new BadRequestException("Anime Not Found"));
     }
     public Anime save(AnimePostRequestBodyDTO animePostDTO) {
         Anime anime = AnimeMapper.INSTANCE.toAnime(animePostDTO);
